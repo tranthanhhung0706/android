9bcba849e5937ace11a655e1291c8df89277e95b
Danh Nguyễn Văn <danh290313@gmail.com>
2023-02-13 23:30:09 +0700

    test13

--- app/src/main/java/com/example/app_movie_ticket/Activity/UserProFilingActivity.java
+++ app/src/main/java/com/example/app_movie_ticket/Activity/UserProFilingActivity.java
@@ -3,15 +3,18 @@ package com.example.app_movie_ticket.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.app_movie_ticket.R;
 
 public class UserProFilingActivity extends AppCompatActivity {
     Button btn_horor;
+    ImageView img_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,9 +32,23 @@ public class UserProFilingActivity extends AppCompatActivity {
                 btn_horor.setTextColor(Color.WHITE);
             }
         });
+
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProFilingActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
+
     private void setControlser() {
+
         btn_horor=findViewById(R.id.btn_horor);
+        img_back = findViewById(R.id.img_back);
+
+
     }
 }
